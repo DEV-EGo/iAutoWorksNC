249275ed66077794341daf1ae850a9bcfae94c70
Edwin <dev.edwing@gmail.com>
2021-04-15 19:11:38 -0400

    email works, js works

--- Resources/javascript/manifest.js
+++ Resources/javascript/manifest.js
@@ -1,11 +1,12 @@
 // formspree JS boilerplate set up
 // added iD of "my-form" onto html next to "post"
+
 var form = document.getElementById("my-form");
     
     async function handleSubmit(event) {
       event.preventDefault();
 
-      var status = document.getElementById("my-form-status");
+      var status = document.getElementById("status");
       var data = new FormData(event.target);
 
       fetch(event.target.action, {
@@ -17,10 +18,12 @@ var form = document.getElementById("my-form");
         }
 
       }).then(response => {
+        form.reset();
+        status.classList.add("sucess");
         status.innerHTML = "Thanks for your submission!";
         form.reset()
       }).catch(error => {
-
+        status.classList.add("error");
         status.innerHTML = "Oops! There was a problem submitting your form"
       });
 
